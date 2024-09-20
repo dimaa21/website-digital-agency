56b5305106f83d96b61daae3174a3abf091bcaa9
Dmytro Haborak <dimasukgaborak@gmail.com>
2024-09-20 18:24:38 +0300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
 document.querySelector('.burger-menu').addEventListener('click', function() {
     document.querySelector('.menu-items').classList.toggle('active');
+    this.classList.toggle('active'); // Додаємо ефект для анімації іконки бургер-меню
 });
+
