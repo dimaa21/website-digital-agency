661b9b0956f5f6a1f8c6375afcf5965bce16dad0
Dmytro Haborak <dimasukgaborak@gmail.com>
2024-11-11 23:30:42 +0200

    Update main.js

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,1 @@
-// main-min.js
-document.addEventListener("DOMContentLoaded", function() {
-    const burger = document.querySelector(".burger");
-    const menu = document.querySelector(".menu");
 
-    burger.addEventListener("click", function() {
-        menu.classList.toggle("active");
-    });
-});
