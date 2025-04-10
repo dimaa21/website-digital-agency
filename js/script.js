e066b163ad8c008f80597e37dac99199c60ea8a1
Dmytro Haborak <dimasukgaborak@gmail.com>
2025-04-10 12:14:55 +0300

    commit upd

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 const burger = document.getElementById('burger-menu');
 const nav = document.getElementById('nav-menu');
+const body = document.body; // Отримуємо body
 
 burger.addEventListener('click', () => {
     nav.classList.toggle('active');
     burger.classList.toggle('open');
+    
+    // Керуємо зміщенням контенту вниз
+    body.classList.toggle('menu-open');
 });
